2605f3f8653fdb8b8afd004333e486879989e026
Rumahnet <149148419+rumahnet@users.noreply.github.com>
2025-11-14 23:27:20 +0700

    Add files via upload

---
+++ components/content/TextRotator.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="text-4xl sm:text-5xl font-bold text-gray-900 dark:text-gray-100">
+    <span class="inline-block min-h-[1.2em]">
+      <span
+        v-for="(text, index) in texts"
+        :key="index"
+        :class="[
+          'inline-block transition-all duration-500',
+          currentIndex === index
+            ? 'opacity-100 translate-y-0'
+            : 'opacity-0 absolute translate-y-4'
+        ]"
+      >
+        {{ text }}
+      </span>
+    </span>
+  </div>
+</template>
+
+<script setup>
+const texts = [
+  'Jasa Sadap WhatsApp',
+  'Jasa Sadap Telegram',
+  'Jasa Sadap Instagram',
+  'Jasa Sadap Facebook',
+  'Jasa Sadap Snapchat'
+]
+
+const currentIndex = ref(0)
+
+onMounted(() => {
+  const interval = setInterval(() => {
+    currentIndex.value = (currentIndex.value + 1) % texts.length
+  }, 3000)
+
+  onUnmounted(() => {
+    clearInterval(interval)
+  })
+})
+</script>
+
---
+++ components/content/img.vue
@@ -0,0 +1,51 @@
+<template>
+  <img
+    :src="src"
+    :alt="alt"
+    :title="title"
+    :class="[
+      'rounded-lg',
+      {
+        'mx-auto': align === 'center',
+        'ml-auto': align === 'right',
+        'mr-auto': align === 'left'
+      }
+    ]"
+    :style="{
+      maxWidth: width ? `${width}px` : '100%',
+      height: height ? `${height}px` : 'auto'
+    }"
+    loading="lazy"
+  />
+</template>
+
+<script setup>
+const props = defineProps({
+  src: {
+    type: String,
+    required: true
+  },
+  alt: {
+    type: String,
+    default: ''
+  },
+  title: {
+    type: String,
+    default: ''
+  },
+  width: {
+    type: [String, Number],
+    default: null
+  },
+  height: {
+    type: [String, Number],
+    default: null
+  },
+  align: {
+    type: String,
+    default: 'center',
+    validator: (value) => ['left', 'center', 'right'].includes(value)
+  }
+})
+</script>
+
